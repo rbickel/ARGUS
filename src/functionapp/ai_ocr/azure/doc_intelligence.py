f68d651b1035921918f3d1fb2f59f626ce89fb7a
Raphael B <raphael.bickel@microsoft.com>
2025-03-07 10:22:22 +0100

    Add backward compatible fix to doc intelligence authN

--- src/functionapp/ai_ocr/azure/doc_intelligence.py
+++ src/functionapp/ai_ocr/azure/doc_intelligence.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 from azure.core.credentials import AzureKeyCredential
+from azure.identity import DefaultAzureCredential
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.ai.documentintelligence.models import DocumentAnalysisFeature
 from ai_ocr.azure.config import get_config
@@ -8,9 +9,13 @@ from ai_ocr.azure.config import get_config
 
 config = get_config()
 
+credential = DefaultAzureCredential()
+if(config["doc_intelligence_key"] != ""):
+    credential = AzureKeyCredential(config["doc_intelligence_key"])
+
 document_intelligence_client = DocumentIntelligenceClient(endpoint=config["doc_intelligence_endpoint"],
-                                                               credential=AzureKeyCredential(config["doc_intelligence_key"]),
-                                                               headers={"solution":"ARGUS-1.0"})
+                                                            credential=AzureKeyCredential(config["doc_intelligence_key"]),
+                                                            headers={"solution":"ARGUS-1.0"})
 
 def get_ocr_results(file_path: str):
     with open(file_path, "rb") as f:
